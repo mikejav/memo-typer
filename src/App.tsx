471c50ecc6ebc20437418494f73ec0417bb2e5ae
Michal Jaworski <mikejav@users.noreply.github.com>
2019-12-02 18:40:50 +0100

    add app basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  LayoutWrapper,
+  LayoutNavbar,
+  LayoutContent,
+  Container
+} from 'components';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LayoutWrapper>
+      <LayoutNavbar>
+        <Container>layoutNavbar</Container>
+      </LayoutNavbar>
+      <LayoutContent>
+        <Container>Layout content</Container>
+      </LayoutContent>
+    </LayoutWrapper>
   );
-}
+};
 
 export default App;
