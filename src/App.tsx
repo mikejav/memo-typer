04abaebdd39cd1cae9cb59ccb1c14c541ce292c7
Michal Jaworski <mikejav@users.noreply.github.com>
2019-12-03 18:49:23 +0100

    add level list and learn pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,29 @@ import {
   LayoutContent,
   Container
 } from 'components';
+import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom';
+import { LevelList, Learn } from 'pages';
 
 const App: React.FC = () => {
   return (
-    <LayoutWrapper>
-      <LayoutNavbar>
-        <Container>layoutNavbar</Container>
-      </LayoutNavbar>
-      <LayoutContent>
-        <Container>Layout content</Container>
-      </LayoutContent>
-    </LayoutWrapper>
+    <BrowserRouter>
+      <LayoutWrapper>
+        <LayoutNavbar>
+          <Container>
+            <Link to="/">TODO: logo & home</Link>
+          </Container>
+        </LayoutNavbar>
+        <LayoutContent>
+          <Container>
+            <Switch>
+              <Route exact path="/" component={LevelList} />
+              <Route path="/learn/:level" component={Learn} />
+              <Redirect to="/" />
+            </Switch>
+          </Container>
+        </LayoutContent>
+      </LayoutWrapper>
+    </BrowserRouter>
   );
 };
 
