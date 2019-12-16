b436fcb7b5be53b567fb0f84f347318eae413b6b
Michal Jaworski <mikejav@users.noreply.github.com>
2019-12-16 18:31:36 +0100

    remove old unused reducer

--- src/state/reducers/lessons.ts
+++
@@ -1,6 +0,0 @@
-import { LessonBasic } from './../models/LessonDetails';
-import { Reducer } from 'redux';
-
-export const lessons: Reducer<LessonBasic[], any> = (state = [], action) => {
-  return state;
-};
