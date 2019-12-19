54ea434feef19af7cd2ede6004b8d830531ee156
Michal Jaworski <mikejav@users.noreply.github.com>
2019-12-19 22:48:30 +0100

    Card component: add margin spacing logic

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,19 @@
 import React, { FC } from 'react';
+import classNames from 'classnames/bind';
 import styles from './Card.module.scss';
+import { useSpacingClassMemo } from 'shared/utils/outer-spacing';
+import { OuterSpacing } from 'shared/utils/outer-spacing/OuterSpacing';
 
-export const Card: FC = ({ children }) => {
-  return <div className={styles.Card}>{children}</div>;
+let cx = classNames.bind(styles);
+
+interface CardProps {
+  margin?: OuterSpacing;
+}
+
+export const Card: FC<CardProps> = ({ margin, children }) => {
+
+  const margins = useSpacingClassMemo(margin);
+  const classNames = cx('Card', margins);
+
+  return <div className={classNames}>{children}</div>;
 };
