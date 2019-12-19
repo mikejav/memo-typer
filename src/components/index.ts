cd6f495aa95d502a4793b3886fffaf9c2b4e1637
Michal Jaworski <mikejav@users.noreply.github.com>
2019-12-19 22:49:11 +0100

    add Card component to barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import { Container } from './Container/Container';
 import { LayoutContent } from './LayoutContent/LayoutContent';
 import { LayoutWrapper } from './LayoutWrapper/LayoutWrapper';
 import { LayoutNavbar } from './LayoutNavbar/LayoutNavbar';
+import { Card } from './Card/Card';
 
 export {
   LayoutContent,
   LayoutNavbar,
   LayoutWrapper,
   Container,
+  Card,
 }
