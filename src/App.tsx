7580fef1fde7062ff0d9414dde58b45c4bc05bc5
Michal Jaworski <mikejav@users.noreply.github.com>
2019-12-21 21:22:58 +0100

    remove custom components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import {
-  Container,
-  LayoutContent,
-  LayoutNavbar,
-  LayoutWrapper
-} from 'components';
 import { Learn, LessonsList } from 'pages';
 import React from 'react';
 import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
@@ -11,22 +5,15 @@ import { BrowserRouter, Link, Redirect, Route, Switch } from 'react-router-dom';
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <LayoutWrapper>
-        <LayoutNavbar>
-          <Container>
+
             <Link to="/">TODO: logo & home</Link>
-          </Container>
-        </LayoutNavbar>
-        <LayoutContent>
-          <Container>
+
             <Switch>
               <Route exact path="/" component={LessonsList} />
               <Route path="/learn/:levelName" component={Learn} />
               <Redirect to="/" />
             </Switch>
-          </Container>
-        </LayoutContent>
-      </LayoutWrapper>
+
     </BrowserRouter>
   );
 };
