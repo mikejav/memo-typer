d25a96ccf7b7cd099130c51d0ba407c1bb4b0f63
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-03 17:45:34 +0100

    add LessonOverView to barrel

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
-import { Learn } from './learn/learn';
+import { LessonOverview } from 'pages/lesson-overview/lesson-overview';
 import { LessonsList } from 'pages/level-list/lessons-list';
 
 export {
   LessonsList,
-  Learn,
+  LessonOverview,
 };
--- src/pages/learn/learn.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from 'react';
-
-export const Learn: FC = () => {
-  return <div>LEARN</div>;
-};
