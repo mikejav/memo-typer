0282ea982772b1c4ed26d9d91a05a5836ba31b8f
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-03 17:44:19 +0100

    update lessons list

--- src/pages/level-list/lessons-list.tsx
+++ src/pages/level-list/lessons-list.tsx
@@ -1,17 +1,34 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useLessonsSelector } from 'selectors';
+import { Container } from '@material-ui/core';
+import { LessonCard } from 'pages/level-list/components/LessonCard';
+import Grid from '@material-ui/core/Grid';
+import { LessonsEffects } from 'state/lessons/lessons.effects';
+import { useDispatch } from 'react-redux';
 
 export const LessonsList: FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(LessonsEffects.loadLessonsIfNeeded());
+  }, []);
+
   const lessons = useLessonsSelector();
 
   return (
-    <Fragment>
-      {
-        lessons.isFetching
-          ? <div>loading...</div>
-          : lessons.lessonsList.map(lesson => lesson.name)
-      }
-    </Fragment>
+    <Container maxWidth="md">
+      {lessons.isFetching ? (
+        <div>Loading...</div>
+      ) : (
+        <Grid container spacing={2}>
+          {lessons.lessonsList.map(lesson => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={lesson.id}>
+              <LessonCard lesson={lesson}/>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Container>
   );
   // return <div>level list</div>;
 };
