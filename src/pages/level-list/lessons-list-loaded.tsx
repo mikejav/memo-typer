69cd3ffe1c2780756ed5a83a31b8e5c000f386f1
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-10 20:44:51 +0100

    update template

--- src/pages/level-list/lessons-list-loaded.tsx
+++ src/pages/level-list/lessons-list-loaded.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { useLessonsSelector } from 'selectors';
-import { Container } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import { LessonCard } from 'pages/level-list/components/LessonCard';
 
@@ -8,15 +7,12 @@ export const LessonsListLoaded: FC = () => {
   const lessons = useLessonsSelector();
 
   return (
-    <Container maxWidth="md">
-      <Grid container spacing={2}>
-        {console.log('ten widok powinien sie ulepic tylko raz')}
-        {lessons.lessonsList.map(lesson => (
-          <Grid item style={{ display: 'flex' }} xs={12} sm={6} md={4} key={lesson.id}>
-            <LessonCard lesson={lesson}/>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Grid container spacing={2}>
+      {lessons.lessonsList.map(lesson => (
+        <Grid item style={{ display: 'flex' }} xs={12} sm={6} md={4} key={lesson.id}>
+          <LessonCard lesson={lesson}/>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
