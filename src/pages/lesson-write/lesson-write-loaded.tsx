103ca13b81e5118123f1fb38ee7049c9da401c42
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-10 20:51:56 +0100

    add lesson-write-loaded

---
+++ src/pages/lesson-write/lesson-write-loaded.tsx
@@ -0,0 +1,49 @@
+import React, { FC, Fragment, useState } from 'react';
+import { useSelectedLessonSelector } from 'selectors';
+import { WriteCard } from 'pages/lesson-write/components/WriteCard';
+import Card from '@material-ui/core/Card';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import CardHeader from '@material-ui/core/CardHeader';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+import { RouterLink } from 'shared/components';
+import Typography from '@material-ui/core/Typography';
+
+export const LessonWriteLoaded: FC = () => {
+  const { lessonDetails } = useSelectedLessonSelector();
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const currentDefinitionPercentage = (currentPhraseIndex + 1) / (lessonDetails.phrases.length + 1) * 100;
+
+  return (
+    <Fragment>
+      {/*TODO: breadcrumbs to component*/}
+      <Breadcrumbs>
+        <Link color="inherit" component={RouterLink} to='/'>
+          Home
+        </Link>
+        <Link color="inherit" component={RouterLink} to={`/lesson/${lessonDetails.id}`}>
+          {lessonDetails.name}
+        </Link>
+        <Typography color="textPrimary">Pisz</Typography>
+      </Breadcrumbs>
+      {lessonDetails.phrases.length && (
+        <Card style={{ textAlign: 'center' }}>
+          {currentPhraseIndex === lessonDetails.phrases.length ? (
+            <span>U R awesome!</span>
+          ) : (
+            <Fragment>
+              <LinearProgress variant='determinate' value={currentDefinitionPercentage}/>
+              <CardHeader subheader={`${currentPhraseIndex + 1} z ${lessonDetails.phrases.length}`}/>
+              <WriteCard
+                phrase={lessonDetails.phrases[currentPhraseIndex]}
+                onPhraseTypeSuccess={() => setCurrentPhraseIndex(i => i + 1)}
+              />
+            </Fragment>
+          )}
+
+        </Card>
+      )}
+    </Fragment>
+
+  );
+};
