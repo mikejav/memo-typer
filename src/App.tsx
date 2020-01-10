6753cf8b62f8b01b7b71701e3e382cf51a65ace1
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-10 20:50:09 +0100

    add main content container

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import { LessonOverview, LessonsList } from 'pages';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { AppBar, CssBaseline, ThemeProvider } from '@material-ui/core';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { RouterLink } from 'shared/components/RouterLink/RouterLink';
+import { Container, CssBaseline, ThemeProvider } from '@material-ui/core';
 import { LessonWrite } from 'pages/lesson-write/lesson-write';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import Menu from '@material-ui/core/Menu';
-import Button from '@material-ui/core/Button';
-import { AccountCircle } from '@material-ui/icons';
 import Header from 'layout/header';
 
 // TODO: move theme to another file
@@ -34,14 +25,14 @@ const App: React.FC = () => {
         <BrowserRouter>
           <Header/>
 
-          <div style={{ paddingTop: '1rem' }}>
+          <Container maxWidth="md" style={{ paddingTop: '1rem' }}>
             <Switch>
               <Route exact path="/" component={LessonsList}/>
               <Route path="/lesson/:lessonId" component={LessonOverview} exact/>
               <Route path="/lesson/:lessonId/write" component={LessonWrite} exact/>
               <Redirect to="/"/>
             </Switch>
-          </div>
+          </Container>
 
         </BrowserRouter>
       </ThemeProvider>
