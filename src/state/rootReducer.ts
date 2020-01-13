bc7c13f5299614b19586c62a7d1bf8a2997d1e07
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-13 19:16:27 +0100

    add layout reducer

--- src/state/rootReducer.ts
+++ src/state/rootReducer.ts
@@ -1,47 +1,12 @@
 import { combineReducers } from 'redux';
 import { selectedLessonReducer } from 'state/selectedLesson/selected-lesson.reducer';
 import { lessonsReducer } from 'state/lessons/lessons.reducer';
+import { layoutReducer } from 'state/layout/layout.reducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export const rootReducer = combineReducers({
   lessons: lessonsReducer,
-  selectedLesson: selectedLessonReducer
+  selectedLesson: selectedLessonReducer,
+  layout: layoutReducer,
 });
-
-
-/*  
-
-modele:
-
-LessonBasic:
-  id: string,
-  name: string,
-  description: string,
-
-LessonDetails:
-  id: string,
-  name: string,
-  description: string,
-  phrases: [ // lista słówek / fraz
-    {id: string, content: string}
-  ]
-
- {
-   lessons: [
-      {
-        id: string,
-        name: string,
-        description: string,
-        phrases: [ // lista słówek / fraz
-          {id: string, content: string}
-        ]
-      }
-   ],
-
-   progress: [
-     good: number,
-     bad: number,
-   ]
- }
- */
