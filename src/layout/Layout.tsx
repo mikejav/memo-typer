4daa6bc8284b391d9d09129e136299222f534f67
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-14 18:23:23 +0100

    add LayoutContainer flex styling

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,10 +5,20 @@ import { LayoutMain } from 'layout/LayoutMain';
 import { LayoutSidenavDesktop } from 'layout/LayoutSidenavDesktop';
 import { LayoutMainContent } from 'layout/LayoutMainContent';
 import { LayoutContainer } from 'layout/LayoutContainer';
-import { Container, useMediaQuery, useTheme } from '@material-ui/core';
+import { Container, createStyles, makeStyles, Theme, useMediaQuery, useTheme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      paddingTop: '1rem',
+      flexGrow: 1
+    }
+  })
+);
 
 export const Layout: FC = ({ children }) => {
   const theme = useTheme();
+  const classes = useStyles();
   const isDownSmallDevice = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
@@ -18,7 +28,7 @@ export const Layout: FC = ({ children }) => {
       <LayoutMain>
         <LayoutSidenavDesktop/>
         <LayoutMainContent>
-          <Container maxWidth="md" style={{ paddingTop: '1rem' }}>
+          <Container maxWidth="md" className={classes.container}>
             {children}
           </Container>
         </LayoutMainContent>
