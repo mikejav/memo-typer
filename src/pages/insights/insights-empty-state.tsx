b4e1b5085462357cc5b39ee2e84f7334767dc93d
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-16 18:20:52 +0100

    update insights-empty-state template

--- src/pages/insights/insights-empty-state.tsx
+++ src/pages/insights/insights-empty-state.tsx
@@ -1,8 +1,29 @@
 import React, { FC } from 'react';
+import {
+  EmptyState,
+  EmptyStateActionPrimary,
+  EmptyStateActions,
+  EmptyStateActionSecondary,
+  EmptyStatePicture,
+  EmptyStateTextPrimary,
+  EmptyStateTextSecondary
+} from 'shared/components/EmptyState';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import { Button } from '@material-ui/core';
+
 
 export const InsightsEmptyState: FC = () => {
-
   return (
-    <div>InsightsEmptyState component</div>
+    <EmptyState>
+      <EmptyStatePicture><TimelineIcon/></EmptyStatePicture>
+      <EmptyStateTextPrimary>Empty insights</EmptyStateTextPrimary>
+      <EmptyStateTextSecondary>You have no statistic data</EmptyStateTextSecondary>
+      <EmptyStateActions>
+        <EmptyStateActionPrimary>
+          <Button variant={'contained'} color={'primary'}>Primary action</Button>
+        </EmptyStateActionPrimary>
+        <EmptyStateActionSecondary>TODO: secondary action eg link</EmptyStateActionSecondary>
+      </EmptyStateActions>
+    </EmptyState>
   );
 };
