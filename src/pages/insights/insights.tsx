ae40b16a1ee6e08f1eb3837f901ceb4936109d39
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-17 18:23:50 +0100

    update empty state styling

--- src/pages/insights/insights.tsx
+++ src/pages/insights/insights.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useCallback } from 'react';
 import { Load } from 'shared/components';
 import { useDispatch } from 'react-redux';
-import { useInsightsSelector } from 'selectors';
 import { InsightsEffects } from 'state/insights/insights.effects';
 import { InsightsLoading } from 'pages/insights/insights-loading';
 import { InsightsLoaded } from 'pages/insights/insights-loaded';
