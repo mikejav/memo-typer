179f10a1aeccbdd316b136403862ffca3b5de5a3
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-20 18:30:39 +0100

    add BreadcrumbsLoading component

---
+++ src/shared/components/BreadcrumbsLoading/BreadcrumbsLoading.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { times } from 'lodash';
+import { Skeleton } from '@material-ui/lab';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      marginBottom: '1rem'
+    },
+    part: {
+      height: 24,
+      '&:not(:first-child)': {
+        marginLeft: theme.spacing(2)
+      }
+    }
+  })
+);
+
+const partsWidthPercentage = [10, 20, 10, 10];
+
+interface BreadcrumbsLoadingProps {
+  partsNumber: 1 | 2 | 3 | 4;
+}
+
+export const BreadcrumbsLoading: FC<BreadcrumbsLoadingProps> = ({ partsNumber }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {times(partsNumber, i => (
+        <Skeleton
+          variant='text'
+          key={i}
+          width={`${partsWidthPercentage[i]}%`}
+          className={classes.part}
+        />
+      ))}
+    </div>
+  );
+};
