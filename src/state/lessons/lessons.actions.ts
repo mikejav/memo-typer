fb569f3c437b1a7018d9b0d861a414f1762825bd
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-29 22:26:08 +0100

    remove old lessons state

--- src/state/lessons/lessons.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-import { LessonBasic } from 'models/LessonBasic';
-
-export abstract class LessonsActions {
-  public static readonly loadRequest = createAction('lessons/LOAD_REQUEST');
-  public static readonly loadSuccess = createAction<{[id: string]: LessonBasic}>('lessons/LOAD_SUCCESS');
-  public static readonly loadFailure = createAction('lessons/LOAD_FAILURE');
-}
--- src/state/lessons/lessons.selectors.ts
+++
@@ -1,4 +0,0 @@
-import { useSelector } from 'react-redux';
-import { RootState } from 'state/rootReducer';
-
-export const useLessonsSelector = () => useSelector((state: RootState) => state.lessons);
