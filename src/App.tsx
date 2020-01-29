da35206fff1316a419dc7ccac92ec4b4dedbdb5c
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-29 22:29:00 +0100

    move MUI theme to its file

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,8 @@ import { LessonWrite } from 'pages/lesson-write/lesson-write';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import { Layout } from 'layout';
 import { Insights } from 'pages/insights/insights';
-
-// TODO: move theme to another file
-const theme = createMuiTheme({
-  overrides: {
-    MuiBreadcrumbs: {
-      root: {
-        marginBottom: '1rem'
-      }
-    }
-  }
-});
+import { muiTheme } from 'mui-theme';
+import { FirebaseApp } from 'state/firebaseApp';
 
 const App: React.FC = () => {
   return (
