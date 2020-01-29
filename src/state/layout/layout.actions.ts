695f762c1fa27aad5f2c14f53707309439a4b545
Michal Jaworski <mikejav@users.noreply.github.com>
2020-01-29 22:23:54 +0100

    update LayoutActions: add 'readonly' keyword

--- src/state/layout/layout.actions.ts
+++ src/state/layout/layout.actions.ts
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export abstract class LayoutActions {
-  public static openMobileSidenav = createAction('layout/OPEN_MOBILE_SIDENAV');
-  public static closeMobileSidenav = createAction('layout/CLOSE_MOBILE_SIDENAV');
-  public static toggleMobileSidenav = createAction('layout/TOGGLE_MOBILE_SIDENAV');
+  public static readonly openMobileSidenav = createAction('layout/OPEN_MOBILE_SIDENAV');
+  public static readonly closeMobileSidenav = createAction('layout/CLOSE_MOBILE_SIDENAV');
+  public static readonly toggleMobileSidenav = createAction('layout/TOGGLE_MOBILE_SIDENAV');
 }
