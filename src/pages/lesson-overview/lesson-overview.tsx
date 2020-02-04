2776692d3b6eb9c54b7583f496e77297641eddc3
Michal Jaworski <mikejav@users.noreply.github.com>
2020-02-04 23:17:07 +0100

    lesson-overview: update logic, rename some stuff

--- src/pages/lesson-overview/lesson-overview.tsx
+++ src/pages/lesson-overview/lesson-overview.tsx
@@ -1,28 +1,48 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, Fragment, useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { SelectedLessonEffects } from 'state/selectedLesson/selected-lesson.effects';
 import { useDispatch } from 'react-redux';
 import { Load } from 'shared/components';
-import { LessonOverviewLoaded } from 'pages/lesson-overview/lesson-overview-loaded';
-import { LessonOverviewLoading } from 'pages/lesson-overview/lesson-overview-loading';
-import { useSelectedLessonDataSelector } from 'state/selectedLesson/selected-lesson.selectors';
+import { LessonOverviewLoaded } from 'pages/lesson-overview/subject-overview/lesson-overview-loaded';
+import { LessonOverviewLoading } from 'pages/lesson-overview/subject-overview/lesson-overview-loading';
+import {
+  useSubjectPagePhrasesSelector,
+  useSubjectPageSubjectSelector
+} from 'state/pages/subject-page/subject-page.selectors';
+import { SubjectPageService } from 'state/pages/subject-page/subject-page.service';
+import { SubjectPageActions } from 'state/pages/subject-page/subject-page.actions';
+import { SubjectPhrasesLoading } from 'pages/lesson-overview/subject-phrases/subject-phrases-loading';
+import { SubjectPhrasesLoaded } from 'pages/lesson-overview/subject-phrases/subject-phrases-loaded';
 
 
 export const LessonOverview: FC = () => {
   const dispatch = useDispatch();
-  const { lessonId } = useParams();
-  const selectedLessonDetails = useSelectedLessonDataSelector();
+  // TODO: zmiana nazyw i w routingu też
+  const { lessonId: subjectId } = useParams();
+  const selectedSubject = useSubjectPageSubjectSelector();
+  const selectedSubjectPhrases = useSubjectPagePhrasesSelector();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const loadAction = useCallback(() => dispatch(SelectedLessonEffects.loadSelectedLesson(lessonId!)), []);
-  const cancelLoading = useCallback(() => console.log('TODO: loadingcancelation method: WIn win win!!2'), []);
+  const loadSubjectCallback = useCallback(() => dispatch(SubjectPageService.loadSubject(subjectId!)), []);
+  const cancelSubjectLoadingCallback = useCallback(() => console.log('TODO: loadingcancelation method: WIn win win!!2'), []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const loadSubjectPhrasesCallback = useCallback(() => dispatch(SubjectPageService.loadSubjectPhrases(subjectId!)), []);
+  const cancelSubjectPhrasesLoadingCallback = useCallback(() => console.log('TODO: loadingcancelation method: WIn win win!!22'), []);
 
   return (
-    <Load
-      isLoaded={selectedLessonDetails.id === lessonId}
-      loadAction={loadAction}
-      cancelLoading={cancelLoading}
-      LoadingComponent={LessonOverviewLoading}
-      LoadedComponent={LessonOverviewLoaded}
-    />
+    <Fragment>
+      <Load
+        isLoaded={selectedSubject?.id === subjectId}
+        loadAction={loadSubjectCallback}
+        cancelLoading={cancelSubjectLoadingCallback}
+        LoadingComponent={LessonOverviewLoading}
+        LoadedComponent={LessonOverviewLoaded}
+      />
+      <Load
+        isLoaded={selectedSubjectPhrases}
+        loadAction={loadSubjectPhrasesCallback}
+        cancelLoading={cancelSubjectPhrasesLoadingCallback}
+        LoadingComponent={SubjectPhrasesLoading}
+        LoadedComponent={SubjectPhrasesLoaded}
+      />
+    </Fragment>
   );
 };
