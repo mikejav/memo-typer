2f2f0693d0baa11306fe4fa01d22f6168ac92dca
Michal Jaworski <mikejav@users.noreply.github.com>
2020-02-06 20:49:14 +0100

    update subject-write logic

--- src/pages/lesson-write/lesson-write-loaded.tsx
+++ src/pages/lesson-write/lesson-write-loaded.tsx
@@ -9,13 +9,17 @@ import { RouterLink } from 'shared/components';
 import Typography from '@material-ui/core/Typography';
 import { useSelectedLessonSelector } from 'state/selectedLesson/selected-lesson.selectors';
 import { CardContent } from '@material-ui/core';
+import {
+  useSubjectPagePhrasesSelector,
+  useSubjectPageSubjectSelector
+} from 'state/pages/subject-page/subject-page.selectors';
 
 export const LessonWriteLoaded: FC = () => {
-  // TODO: selektor!!
-  const selectedLesson = useSelectedLessonSelector();
-  const lessonDetails = selectedLesson.data;
+  // TODO: selektor lub przekazywanie danych z parenta!!
+  const selectedLesson = useSubjectPageSubjectSelector();
+  const selectedLessonPhrases = useSubjectPagePhrasesSelector()!;
   const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
-  const currentDefinitionPercentage = (currentPhraseIndex + 1) / (lessonDetails.phrases.length + 1) * 100;
+  const currentDefinitionPercentage = (currentPhraseIndex + 1) / (selectedLessonPhrases.length + 1) * 100;
 
   return (
     <Fragment>
@@ -24,23 +28,23 @@ export const LessonWriteLoaded: FC = () => {
         <Link color="inherit" component={RouterLink} to='/'>
           Home
         </Link>
-        <Link color="inherit" component={RouterLink} to={`/lesson/${lessonDetails.id}`}>
-          {lessonDetails.name}
+        <Link color="inherit" component={RouterLink} to={`/lesson/${selectedLesson.id}`}>
+          {selectedLesson.name}
         </Link>
         <Typography color="textPrimary">Pisz</Typography>
       </Breadcrumbs>
-      {lessonDetails.phrases.length && (
+      {selectedLessonPhrases.length && (
         <Card style={{ textAlign: 'center' }}>
-          {currentPhraseIndex === lessonDetails.phrases.length ? (
+          {currentPhraseIndex === selectedLessonPhrases.length ? (
             <CardContent>
               TODO: U R awesome content! Nie jesteś zalogowany. ZALOGUJ SIĘ aby śledzić swoje postępy
             </CardContent>
           ) : (
             <Fragment>
               <LinearProgress variant='determinate' value={currentDefinitionPercentage}/>
-              <CardHeader subheader={`${currentPhraseIndex + 1} z ${lessonDetails.phrases.length}`}/>
+              <CardHeader subheader={`${currentPhraseIndex + 1} z ${selectedLessonPhrases.length}`}/>
               <WriteCard
-                phrase={lessonDetails.phrases[currentPhraseIndex]}
+                phrase={selectedLessonPhrases[currentPhraseIndex]}
                 onPhraseTypeSuccess={() => setCurrentPhraseIndex(i => i + 1)}
               />
             </Fragment>
--- src/pages/lesson-write/lesson-write.tsx
+++ src/pages/lesson-write/lesson-write.tsx
@@ -1,25 +1,46 @@
 import React, { FC, useCallback } from 'react';
 import { useParams } from 'react-router';
-import { useDispatch } from 'react-redux';
-import { SelectedLessonEffects } from 'state/selectedLesson/selected-lesson.effects';
+import { batch, useDispatch } from 'react-redux';
 import { Load } from 'shared/components';
 import { LessonWriteLoaded } from 'pages/lesson-write/lesson-write-loaded';
 import { LessonWriteLoading } from 'pages/lesson-write/lesson-write-loading';
-import { useSelectedLessonSelector } from 'state/selectedLesson/selected-lesson.selectors';
+import { SubjectPageService } from 'state/pages/subject-page/subject-page.service';
+import {
+  useSubjectPagePhrasesSelector,
+  useSubjectPageSubjectSelector
+} from 'state/pages/subject-page/subject-page.selectors';
 
 export const LessonWrite: FC = () => {
   const dispatch = useDispatch();
   const { lessonId } = useParams();
   // TODO: selektor!!
-  const selectedLesson = useSelectedLessonSelector();
-  const lessonDetails = selectedLesson.data;
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const loadAction = useCallback(() => dispatch(SelectedLessonEffects.loadSelectedLesson(lessonId!)), []);
+  const selectedLesson = useSubjectPageSubjectSelector();
+  const selectedSubjectPhrases = useSubjectPagePhrasesSelector();
+
+  const isLessonLoaded = selectedLesson?.id === lessonId;
+  const arePhrasesLoaded = !!selectedSubjectPhrases;
+
+  // TODO: można by się pokusić żeby to ogarnąć w ten sposób,
+  //  że do Load komponentu przekazuje tablice tych rzeczy ewentualnie,
+  //  i wtedy to by działalo jak taki Angularowy resolver,
+  //  z tą różnicą że przechodziłby na podstrone od razu oczywiście,
+  //  co jest lepiej rozwiązane IMO nie w Angularze
+  const loadAction = useCallback(() => {
+    batch(() => {
+      if (!isLessonLoaded) {
+        dispatch(SubjectPageService.loadSubject(lessonId!));
+      }
+      if (!arePhrasesLoaded) {
+        dispatch(SubjectPageService.loadSubjectPhrases(lessonId!));
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   const cancelLoading = useCallback(() => console.log('TODO: loadingcancelation method: WIn win win!!3'), []);
 
   return (
     <Load
-      isLoaded={lessonDetails.id === lessonId}
+      isLoaded={isLessonLoaded && arePhrasesLoaded}
       loadAction={loadAction}
       cancelLoading={cancelLoading}
       LoadingComponent={LessonWriteLoading}
