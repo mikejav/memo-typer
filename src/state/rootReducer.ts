0fc270c7b15b1bc61e5cc6d10747962b623ad54f
Michal Jaworski <mikejav@users.noreply.github.com>
2020-02-06 22:51:05 +0100

    update reducer

--- src/state/rootReducer.ts
+++ src/state/rootReducer.ts
@@ -3,7 +3,6 @@ import { subjectsReducer } from 'state/entities/subjects/subjects.reducer';
 import { layoutReducer } from 'state/layout/layout.reducer';
 import { insightsReducer } from 'state/insights/insights.reducer';
 import { subjectsPageReducer } from 'state/pages/subjects-page/subjects-page.reducer';
-import { selectedLessonReducer } from 'state/selectedLesson/selected-lesson.reducer';
 import { subjectPageReducer } from 'state/pages/subject-page/subject-page.reducer';
 import { phrasesReducer } from 'state/entities/phrases/phrases.reducer';
 
@@ -12,12 +11,12 @@ export type RootState = ReturnType<typeof rootReducer>;
 export const rootReducer = combineReducers({
   entities: combineReducers({
     subjects: subjectsReducer,
-    phrases: phrasesReducer,
+    phrases: phrasesReducer
   }),
   pages: combineReducers({
     subjects: subjectsPageReducer,
-    subject: subjectPageReducer,
+    subject: subjectPageReducer
   }),
   layout: layoutReducer,
-  insights: insightsReducer,
+  insights: insightsReducer
 });
