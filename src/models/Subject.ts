a465ab29007c4e0732df110c7d76ce68e7d5335a
Michal Jaworski <mikejav@users.noreply.github.com>
2020-02-17 23:02:14 +0100

    Subject model: add createdAt and Phrases fields

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,6 +1,11 @@
+import { Phrase } from 'models/Phrase';
+import firebase from 'firebase/app';
+
 export interface Subject {
   id: string;
   name: string;
   description: string;
   coverLink: string;
+  createdAt: firebase.firestore.Timestamp;
+  phrases?: Phrase[];
 }
